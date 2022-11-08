9cf8b85b79f50e929f7098a6f44f3239a6932e70
w-omar <wocollado@gmail.com>
2022-11-08 15:32:30 -0500

    graphql setup and CRUD for recipe

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const mongoose = require('mongoose');
+const { ApolloServer } = require( 'apollo-server');
 
-const app = express();
+require("dotenv").config();
+
+/*const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors());*/
 
-const CONNECTION_URL = 'mongodb+srv://cobraeats:cobraeats@cluster0.lch3daa.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const port = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({req}) => ({req})
+});
+
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+      console.log("MongoDB connected successfully");
+      return server.listen({port});
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`)
+  })
   .catch((error) => console.log(`${error} did not connect`));
