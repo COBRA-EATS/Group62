1c89ac232c1bacc841ac8f6a53717f7c44927a27
raderbrad <92053204+raderbrad@users.noreply.github.com>
2022-12-06 02:17:41 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,20 @@ import {Box, Container} from '@mui/material';
 
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
-import { AuthProvider } from './context/auth';
+import CreatePost from "./components/CreatePost/CreatePost";
 
 const App = () => {
     return (
-        <AuthProvider>
         <BrowserRouter>
         <Box>
             <Navbar/>
                 <Routes>
                     <Route path="/" exact element={<Home/>}/>
                     <Route path="/auth" exact element={<Auth/>}/>
+                    <Route path="/createpost" exact element={<CreatePost/>}/>
                 </Routes>     
         </Box>
         </BrowserRouter>
-        </AuthProvider>
     );
 }
 
