be2ef6c2c966f001c61ad94d7fee7d032f707eb8
raderbrad <92053204+raderbrad@users.noreply.github.com>
2022-12-06 17:39:03 -0500

    posts are centered

--- client/src/components/Home/Feed.js
+++ client/src/components/Home/Feed.js
@@ -5,7 +5,7 @@ import Post from '../Post/Post'
 const Feed = () => {
     return (
         <Box bgcolor="lightslategrey" flex={4} p={2}>Feed
-            <Stack direction="column" spacing={1} justifyContent="center">
+            <Stack direction="column" spacing={0} justifyContent="center">
                 <Post></Post>
                 <Post></Post>
             </Stack>
