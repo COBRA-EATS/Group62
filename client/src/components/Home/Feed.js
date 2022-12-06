1daf22a2703292d4450c17d6c6915a1176b49dd4
raderbrad <92053204+raderbrad@users.noreply.github.com>
2022-12-06 18:54:47 -0500

    fail

--- client/src/components/Home/Feed.js
+++ client/src/components/Home/Feed.js
@@ -1,17 +1,32 @@
+import { useQuery } from '@apollo/client';
 import { Box, Stack} from '@mui/material';
 import React from 'react'
 import Post from '../Post/Post'
-
+import gql from 'graphql-tag'
 const Feed = () => {
+    const {loading, error, data} = useQuery(GET_FEED);
+    console.log(data);
     return (
         <Box bgcolor="lightslategrey" flex={4} p={2}>Feed
             <Stack direction="column" spacing={0} justifyContent="center">
-                <Post></Post>
-                <Post></Post>
+                {data.recipe.map((recipe) => (
+                    <Post></Post>
+                ))}
             </Stack>
         
         </Box>
     )
 }
 
+const GET_FEED = gql`
+    query feed{
+        recipie{
+            id name description ingredients steps
+        }
+    }
+
+`
+
 export default Feed;
+
+
