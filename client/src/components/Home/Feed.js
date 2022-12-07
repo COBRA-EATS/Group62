509522cc4b6f9e98b8411e65e086686f7055feb2
raderbrad <92053204+raderbrad@users.noreply.github.com>
2022-12-06 19:10:03 -0500

    still errors

--- client/src/components/Home/Feed.js
+++ client/src/components/Home/Feed.js
@@ -4,12 +4,12 @@ import React from 'react'
 import Post from '../Post/Post'
 import gql from 'graphql-tag'
 const Feed = () => {
-    const {loading, error, data} = useQuery(GET_FEED);
+    const [data] = useQuery(GET_FEED);
     console.log(data);
     return (
         <Box bgcolor="lightslategrey" flex={4} p={2}>Feed
             <Stack direction="column" spacing={0} justifyContent="center">
-                {data.recipe.map((recipe) => (
+                {data.allFeed.map((recipe) => (
                     <Post></Post>
                 ))}
             </Stack>
@@ -20,7 +20,7 @@ const Feed = () => {
 
 const GET_FEED = gql`
     query feed{
-        recipie{
+        allFeed{
             id name description ingredients steps
         }
     }
